7b02f431bf223e2e4d7e7cd81caeb4f0beff80dd
Anirudh Kailaje <kailaje@seas.upenn.edu>
2024-07-13 17:36:15 -0400

    Done with setbits(x,y,p,n) and Unit Tests (?)

---
+++ 02_UnitTests/testgen.py
@@ -0,0 +1,13 @@
+import numpy as np
+from numpy.random import randint as rn
+import os
+
+if __name__=="__main__":
+    filename = input("Filename: ")
+    if os.path.exists(f"./{filename}.txt"):
+        os.remove(f"./{filename}.txt")
+    for i in range(1, 32):
+        for _ in range(1000//32):
+            with open(f"{filename}.txt", "a") as f:
+                f.write(f"{rn(2**i)} {rn(2**i)} {rn(32)} {rn(32)}\n")
+            
